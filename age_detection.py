ed76ec82f3a2b93cbaebcd5584df90c93f78ec89
Nirmal Shaju <alduinnirmal@gmail.com>
2021-06-19 22:26:23 +0530

    third commit

--- age_detection.py
+++ age_detection.py
@@ -6,6 +6,6 @@ gui= Tk()
 gui.geometry("700x500")
 gui.title("Age Calculator")
 
-photo = PhotoImage(file="mac-os-logo(1).png")
+photo = PhotoImage(file="mac-os.png")
 myimage = Label(image = photo)
 myimage.grid(row=0,column=1)
