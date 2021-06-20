cd42c4498dbe110c2b57b35c4b4de9f028600fc1
Nirmal Shaju <alduinnirmal99@gmail.com>
2021-06-20 23:14:42 +0530

    gui

--- age_detection.py
+++ age_detection.py
@@ -19,34 +19,34 @@ def calculateAge():
     age = today.year - birthDate.year - ((today.month,today.day) < (birthDate.month, birthDate.day))
 
     Label(text=f"{nameValue.get()} your age is {age}").grid(row = 6, column = 1)
-    Label(text="Name").grid(row=1, column=0, padx=90)
-    Label(text="Year").grid(row=2, column=0)
-    Label(text="Month").grid(row=3, column=0)
-    Label(text="Date").grid(row=4, column=0)
+Label(text="Name").grid(row=1, column=0, padx=90)
+Label(text="Year").grid(row=2, column=0)
+Label(text="Month").grid(row=3, column=0)
+Label(text="Date").grid(row=4, column=0)
 
 
-    nameValue = StringVar()
-    yearValue = StringVar()
-    monthValue = StringVar()
-    dayValue = StringVar()
+nameValue = StringVar()
+yearValue = StringVar()
+monthValue = StringVar()
+dayValue = StringVar()
 
 
-    nameEntry = Entry(gui, textvariable = nameValue)
-    yearEntry = Entry(gui, textvariable= yearValue)
-    monthEntry = Entry(gui, textvariable= monthValue)
-    dayEntry = Entry(gui, textvariable= dayValue)
+nameEntry = Entry(gui, textvariable = nameValue)
+yearEntry = Entry(gui, textvariable= yearValue)
+monthEntry = Entry(gui, textvariable= monthValue)
+dayEntry = Entry(gui, textvariable= dayValue)
 
 
-    nameEntry.grid(row=1, coloumn=1, pady=10)
-    yearEntry.grid(row=1, coloumn=1, pady=10)
-    monthEntry.grid(row=1, coloumn=1, pady=10)
-    dayEntry.grid(row=1, coloumn=1, pady=10)
+nameEntry.grid(row=1, coloumn=1, pady=10)
+yearEntry.grid(row=2, coloumn=1, pady=10)
+monthEntry.grid(row=3, coloumn=1, pady=10)
+dayEntry.grid(row=4, coloumn=1, pady=10)
 
 
-    Button(text="Calculate age", command=calculateAge).grid(row=5, coloumn=1, pady=10)
+Button(text="Calculate age", command=calculateAge).grid(row=5, coloumn=1, pady=10)
 
 
-    gui.mainloop()
+gui.mainloop()
 
 
 
